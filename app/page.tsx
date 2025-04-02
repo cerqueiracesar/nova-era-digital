8087bb1326a914821e4360b50712c6c52fd10c17
Rodrigo Cunha <rodcerqcsar@gmail.com>
2025-04-02 20:57:23 -0300

    test: configurei outra rota para teste

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+// app/page.tsx
+import React from 'react';
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Bem-vindo à minha aplicação!</h1>
+      <p>Esta é a página inicial.</p>
+      {/* Você pode adicionar links para outras partes do seu app aqui, por exemplo: */}
+      {/* <Link href="/intro-questionnaire">Ir para o Questionário Inicial</Link> */}
+    </div>
+  );
+}
