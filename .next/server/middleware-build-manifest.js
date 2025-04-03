7bb91c9650243340b3d0fadf985199ab176b801d
Rodrigo Cunha <rodcerqcsar@gmail.com>
2025-04-03 12:13:08 -0300

    update: atualizando imagens

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,22 @@
-globalThis.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-530336dca7e1dc70.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-2535da2808fff825.js","static/chunks/4bd1b696-c1b04fce5ccc2e30.js","static/chunks/684-e3b4c8f51f35b1be.js","static/chunks/main-app-339ac2df2b12ebdd.js"],rootMainFilesTree:{},pages:{"/_app":["static/chunks/webpack-2535da2808fff825.js","static/chunks/framework-3add273de877ba42.js","static/chunks/main-78ed31be35b8636e.js","static/chunks/pages/_app-2139df877030a0ff.js"],"/_error":["static/chunks/webpack-2535da2808fff825.js","static/chunks/framework-3add273de877ba42.js","static/chunks/main-78ed31be35b8636e.js","static/chunks/pages/_error-63ffe835d20cddf4.js"]},ampFirstPages:[]},globalThis.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+globalThis.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "rootMainFilesTree": {},
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,12 @@ export default function HomePage() {
       <h1>Bem-vindo à minha aplicação!</h1>
       <p>Esta é a página inicial.</p>
       <ul>
-        <li><Link href="/intro-questionnaire">Ir para o Questionário Inicial</Link></li>
-        <li><Link href="/topic-selection">Selecionar um Tópico</Link></li>
+        <li><Link href="/intro-questionnaire">Iniciar &#8594;</Link></li>
+        
+        {/* <li><Link href="/topic-selection">Selecionar um Tópico</Link></li>
         <li><Link href="/quiz">Começar o Quiz</Link></li>
         <li><Link href="/final-questionnaire">Questionário Final</Link></li>
-        <li><Link href="/results">Ver Resultados</Link></li>
+        <li><Link href="/results">Ver Resultados</Link></li> */}
       </ul>
     </div>
   );
